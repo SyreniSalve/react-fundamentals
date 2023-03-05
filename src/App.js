ff78799c6efb95b2f4310fdad6723da342665ff0
Simona Liamo <liamosimona@yahoo.com>
2023-03-05 11:43:09 +0200

    Add rendering list

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import "./App.css";
 import ChildrenWithTitile from "./ChildrenWithTitle";
 import { FirstReactComponent } from "./FirstReactComponent";
 import { ConditionalRenderingComponent } from "./ConditionalRenderingComponent";
+import { listOfAllEmenets, filteredList } from "./RenderingLists";
 
 function App() {
+ 
   return (
     <div>
       <h1>Hello world</h1>
       <p>You look beautiful</p>
       <FirstReactComponent />
-      <ChildrenWithTitile title='I am title' children='I am children' />
+      <ChildrenWithTitile title="I am title" children="I am children" />
       <ConditionalRenderingComponent isBlue={true} />
-      <ConditionalRenderingComponent isBlue={false}/>
+      <ConditionalRenderingComponent isBlue={false} />
+      <section>
+        <h2>Full list</h2>
+        <ul>{listOfAllEmenets}</ul>
+      </section>
+      <section>
+      <h2>Filtered list</h2>
+      <ul>{filteredList}</ul>
+      </section>
     </div>
   );
 }

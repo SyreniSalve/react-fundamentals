66a077e38a90c62baf2d0a7a7d4a0efb52aff7af
Simona Liamo <liamosimona@yahoo.com>
2023-03-09 15:10:53 +0200

    add stateful component task

---
+++ src/StatefulComponent34.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export function StatefulComponent34() {
+    const [count, setCount] = useState(0);
+
+    function handleIncrement() {
+        setCount(count + 1);
+    }
+
+    function handleDecrement() {
+        if(count > 0) {
+            setCount(count - 1);
+        }
+    }
+    
+  return (
+    <div>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+
+      <p>
+        <span>Result: {count}</span>
+      </p>
+    </div>
+  );
+}
